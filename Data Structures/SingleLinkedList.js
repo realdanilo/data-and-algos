4784aab58209ce6f781da152e5c36546580b4e1c
Danilo <realdanilo@users.noreply.github.com>
2020-11-27 20:16:48 -0700

    Singlylinkedlist insert

--- Data Structures/SingleLinkedList.js
+++ Data Structures/SingleLinkedList.js
@@ -58,40 +58,64 @@ class SingleLinkedList{
         return head
     }
    unshift(val){
-    //    add to beg
-    if(val == null || val == undefined) return -1
-    const newHead = new Node(val)
-    const body = this.head 
-    newHead.next = body 
-    this.head = newHead
-    this.length++
+        //    add to beg
+        if(val == null || val == undefined) return -1
+        const newHead = new Node(val)
+        const body = this.head 
+        newHead.next = body 
+        this.head = newHead
+        this.length++
+        return newHead
    }
-    remove(n){
-        if(n > this.length) return -1
-        let count = 1
+   get(n){
+        //    find node at index
+        if(n>this.length) return null
+        let count=0
         let current = this.head
-        while(count <n){
-            current = current.next 
+        while(count < n){
             count++
+            current = current.next
         }
-        // still working
-        console.log(current)
-        console.log("to be romved")
-        return 
-    }
-   
-    
+        return current
+   }
+   set(position, value){
+        let node = this.get(position)
+        if(node){
+           node.value = value 
+           return node
+        }
+        return false
+   }
+   insert(position, value){
+       if(position <0 || position > this.length) return false 
+       if(position == this.length) return this.push(value)
+       if(position ==0) return this.unshift(value)
+    // else, index is in the middle
+        let node = this.get(position-1)
+        let temp = node.next
+        let newNode = new Node(value)
+        newNode.next = temp 
+        node.next = newNode 
+        this.length++
+        return  true
+   }
     
 }
 
 
 
 myList =  new SingleLinkedList()
-myList.push(1)
-myList.push(2)
-myList.push(3)
-myList.unshift(0)
-console.log(myList)
+myList.push("Hello")
+myList.push("Good Bye")
+myList.push("!")
+myList.push("<3")
+myList.push(":)")
+
+myList.insert(5,"LAST")
+console.log("------")
+// console.log(myList)
+// console.log(myList.head.next.next.next)
+
 
 
 
