cc0ea8f90d665adbf916a6688b804e8913627487
Danilo <realdanilo@users.noreply.github.com>
2020-12-26 18:22:12 -0700

    DFS Preorder

--- Data Structures/BST.js
+++ Data Structures/BST.js
@@ -80,4 +80,14 @@ class BST{
         }
         return data
     }
+    DFSPreOrder(){
+        let data =[];
+        function traverse(node){
+            data.push(node.value)
+            if(node.left) traverse(node.left) 
+            if(node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return data;
+    }
 }
