115ec67ca671178b7b7bd514ce08aba2f16b9adf
Danilo <realdanilo@users.noreply.github.com>
2021-01-16 21:09:16 -0700

    DBS post order

--- Data Structures/BST.js
+++ Data Structures/BST.js
@@ -90,4 +90,14 @@ class BST{
         traverse(this.root)
         return data;
     }
+    DFSPostOrder(){
+        let data = []
+        function traverse(node){
+            if(node.left) traverse(node.left)
+            if(node.right) traverse(node.right)
+            data.push(node.value)
+        }
+        traverse(this.root)
+        return data 
+    }
 }
