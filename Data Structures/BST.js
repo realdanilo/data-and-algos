e31c962b60707661bb7f5ad69fb013351f438fcb
Danilo <realdanilo@users.noreply.github.com>
2021-01-16 21:19:59 -0700

    DFS InOrder

--- Data Structures/BST.js
+++ Data Structures/BST.js
@@ -100,4 +100,14 @@ class BST{
         traverse(this.root)
         return data 
     }
+    DFSInOrder(){
+        let data = []
+        function traverse(node){
+            if(node.left) traverse(node.left)
+            data.push(node.value)
+            if(node.right) traverse(node.right)
+        }
+        traverse(this.root)
+        return data
+    }
 }
