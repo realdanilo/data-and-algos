9c1e41ca3f32bedc93b1439744cf5eb2b3cc3879
Danilo <realdanilo@users.noreply.github.com>
2021-02-07 16:33:49 -0700

    priority q skeleton

---
+++ Data Structures/PriorityQueue.js
@@ -0,0 +1,21 @@
+class Node{
+    constructor(val, priority){
+        this.value= val
+        this.priority = priority
+    }
+}
+//MinBinaryHeap
+//lower number = critical
+class PriorityQueue{
+    constructor(){
+        values =[]
+    }
+    //insert
+    enqueue(){
+
+    }
+    //removes root, then sinkDown
+    dequeue(){
+
+    }
+}
