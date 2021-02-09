a162702bcd7c4dcac8873fdc033f140078cb3794
Danilo <realdanilo@users.noreply.github.com>
2021-02-09 16:57:42 -0700

    add edges function

--- Data Structures/Graph.js
+++ Data Structures/Graph.js
@@ -5,4 +5,11 @@ class Graph{
     addVertex(vertex){
         this.adjacencyList[vertex] ? null : this.adjacencyList[vertex] = [] 
     }
+    addEdge(vertex1,vertex2){
+        //if vertexes exists
+        if(!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2]) return -1
+        this.adjacencyList[vertex1].push(vertex2)
+        this.adjacencyList[vertex2].push(vertex1)
+        return this.adjacencyList
+    }
 }
