7306e66a237707ebda204fcbc38afc9f75aae5d7
Danilo <realdanilo@users.noreply.github.com>
2021-02-09 17:18:16 -0700

    remove edges and remove vertex functions

--- Data Structures/Graph.js
+++ Data Structures/Graph.js
@@ -12,4 +12,21 @@ class Graph{
         this.adjacencyList[vertex2].push(vertex1)
         return this.adjacencyList
     }
+    removeEdge(vertex1,vertex2){
+        if(!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2]) return -1
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(x => x !== vertex2)
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(x => x !== vertex1)
+        return this.adjacencyList
+
+    }
+    removeVertex(vertex){
+        if(!this.adjacencyList[vertex]) return -1
+        let edges = this.adjacencyList[vertex]
+        for(let i of edges){
+            this.removeEdge(vertex, i)
+        }
+        //delete key
+        delete this.adjacencyList[vertex]
+        return this.adjacencyList
+    }
 }
