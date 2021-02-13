c4ba464ddda59860ee0652e9535500c96b4b8740
Danilo <realdanilo@users.noreply.github.com>
2021-02-12 20:25:34 -0700

    shortest path algo

--- Data Structures/WeightedGraph.js
+++ Data Structures/WeightedGraph.js
@@ -29,4 +29,47 @@ class WeightedGraph{
         this.adjacencyList[vertex1].push({node:vertex2, weight})
         this.adjacencyList[vertex2].push({node:vertex1, weight})
     }
+    //Dijkstra
+    shortestPath(start,end){
+        let shortestDistances = {}
+        let nodes = new PriorityQueue()
+        let shortestPrevious = {}
+        let currentSmallest
+        let path = []
+
+        //initial state
+       for(let vertex in this.adjacencyList){
+           vertex === start ? shortestDistances[vertex] = 0 : shortestDistances[vertex] = Infinity
+           vertex === start ? nodes.enqueue(vertex,0) : nodes.enqueue(vertex,Infinity)
+           shortestPrevious[vertex] = null
+       }
+       
+       while(nodes.values.length){
+           currentSmallest = nodes.dequeue().val // val : "A"
+           if(currentSmallest == end){
+               while(shortestPrevious[currentSmallest]){
+                   path.push(currentSmallest)
+                   currentSmallest = shortestPrevious[currentSmallest]
+               }
+               path.push(start)
+               return path.reverse()
+           }
+           this.adjacencyList[currentSmallest].forEach( neighbor => {
+                //adding paths
+                let potential = shortestDistances[currentSmallest] + neighbor.weight
+                console.log(currentSmallest,neighbor.node)
+
+                //if path is smaller than before, then update to new one
+                //neighbor.node : "B"
+                if(potential < shortestDistances[neighbor.node]){
+                    shortestDistances[neighbor.node] = potential 
+                    shortestPrevious[neighbor.node] = currentSmallest
+                    //enque 
+                    nodes.enqueue(neighbor.node , potential)
+
+                }
+           })
+
+       }
+    }
 }
